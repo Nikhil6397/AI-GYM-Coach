bbc2d96dc96e02f65a217840738d73ab3051a04c
Nikhil <nikhilrathour555@gmail.com>
2026-09-15 22:33:49 +0530

    update stun

--- main.py
+++ main.py
@@ -197,11 +197,52 @@ def main():
             unsafe_allow_html=True,
         )
     else:
+    #     context = webrtc_streamer(
+    #         key="exercise-analysis",
+    #         mode=WebRtcMode.SENDRECV,
+    #         video_processor_factory=VideoProcessorClass,
+    #         rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+    #         media_stream_constraints={
+    #             "video": True,
+    #             "audio": False
+    #         },
+    #         async_processing=True
+    #     )
+
+    #     sync_metrics_update(context)
+
+    #     if context.state.playing:
+    #         time.sleep(0.25)
+    #         st.rerun()
+
+    #     inject_webrtc_styles()
+
+    # st.divider()
+
         context = webrtc_streamer(
             key="exercise-analysis",
             mode=WebRtcMode.SENDRECV,
             video_processor_factory=VideoProcessorClass,
-            rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+            rtc_configuration={
+                "iceServers": [
+                    {"urls": ["stun:stun.l.google.com:19302"]},
+                    {
+                        "urls": "turn:openrelay.metered.ca:80",
+                        "username": "openrelayproject",
+                        "credential": "openrelayproject",
+                    },
+                    {
+                        "urls": "turn:openrelay.metered.ca:443",
+                        "username": "openrelayproject",
+                        "credential": "openrelayproject",
+                    },
+                    {
+                        "urls": "turn:openrelay.metered.ca:443?transport=tcp",
+                        "username": "openrelayproject",
+                        "credential": "openrelayproject",
+                    },
+                ]
+            },
             media_stream_constraints={
                 "video": True,
                 "audio": False
@@ -211,13 +252,20 @@ def main():
 
         sync_metrics_update(context)
 
-        if context.state.playing:
-            time.sleep(0.25)
-            st.rerun()
-
         inject_webrtc_styles()
 
     st.divider()
+
+
+
+
+
+
+
+
+
+
+
 
     st.markdown("#### Workout History")
 
